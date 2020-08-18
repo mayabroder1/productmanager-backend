5c03a7fde7762379d3a7c8f42f500c41b6bd11bb
jenia <jenian@welldone-software.com>
2020-08-18 20:19:32 +0300

    auth+add task modal+sql

--- dataAccess.js
+++ dataAccess.js
@@ -1,95 +1,155 @@
-const projects = [{
-  id: 1,
-  name: 'project no. 1'
-}];
+let mysql = require('mysql');
+const util = require('util');
+const _ = require('lodash');
 
-const projectFiles = [];
 
-const projectRequirements = [{
-  id: '1',
-  projectId: '1',
-  sourceText: 'source text bla blba',
-  content: 'req content'
-}, {
-  id: '2',
-  projectId: '1',
-  sourceText: 'source text 2 bla blba',
-  content: 'req content 2'
-}, {
-  id: '3',
-  projectId: '1',
-  sourceText: 'source text 3 bla blba',
-  content: 'req content 3'
-}];
+let connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'password',
+  database: 'productmanager'
+});
+connection.connect(function(err) {
+  if (err) {
+    return console.error('error: ' + err.message);
+  }
 
-const taskToRequirements = [{
-  taskId: '1',
-  reqId: '1'
-},{
-  taskId: '2',
-  reqId: '1'
-},{
-  taskId: '1',
-  reqId: '2'
-},{
-  taskId: '3',
-  reqId: '1'
-}, ];
+  console.log('Connected to the MySQL server.');
+});
+const query = util.promisify(connection.query).bind(connection);
 
-const projectTasks = [{
-  id: '1',
-  projectId: '1',
-  parentTaskId: null,
-  content: 'task 1',
-  requirementId: '1'
-}, {
-  id: '2',
-  projectId: '1',
-  parentTaskId: '1',
-  content: 'task 2',
-  requirementId: '1'
-}, {
-  id: '3',
-  projectId: '1',
-  parentTaskId: '1',
-  content: 'task 3',
-  requirementId: '2'
-}, {
-  id: '4',
-  projectId: '1',
-  parentTaskId: '2',
-  content: 'task 4',
-  requirementId: '2'
-}, {
-  id: '5',
-  projectId: '1',
-  parentTaskId: null,
-  content: 'task 5',
-  requirementId: '2'
-}, {
-  id: '6',
-  projectId: '1',
-  parentTaskId: '3',
-  content: 'task 6',
-  requirementId: '2'
-}, {
-  id: '7',
-  projectId: '1',
-  parentTaskId: '3',
-  content: 'task 7',
-  requirementId: '2'
-}, {
-  id: '8',
-  projectId: '1',
-  parentTaskId: '6',
-  content: 'task 8',
-  requirementId: '2'
-}];
+// Users
+
+const getUsers = async () => {
+  return await query('SELECT * FROM users');
+};
+
+const verifyUser = async (username) => {
+  return await query('SELECT count(*) as count_users FROM users WHERE username=? ', [username]);
+};
+
+// Projects
+
+const getProjects = async (username) => {
+  return await query('SELECT * FROM projects WHERE username=?', [username]);
+};
+
+const createProject = async (name) => {
+  return await query('INSERT INTO projects (name) VALUES (?)', [name]);
+};
+
+const updateProject = async ({id, name}) => {
+  return await query('UPDATE projects SET name = ? WHERE id = ?', [name, id]);
+};
+
+const getProject = async (id) => {
+  const results = await query('SELECT * FROM projects WHERE id = ?', [id]);
+  return results[0];
+};
+
+const deleteProject = async (id) => {
+  return await query('DELETE FROM projects WHERE id = ?', [id]);
+};
+
+const getProjectFiles = async (projectId) => {
+  return await query('SELECT * FROM project_files WHERE project_id = ?', [projectId]);
+};
+
+const addProjectFile = async ({name, projectId}) => {
+  return await query('INSERT INTO project_files (name, project_id) VALUES (?, ?)', [name, projectId]);
+};
+
+// Requirements
+
+const getProjectRequirements = async (projectId) => {
+  return await query('SELECT * FROM project_requirements WHERE project_id = ?', [projectId]);
+};
+
+const updateRequirement = async (reqId, content) => {
+  await query('UPDATE project_requirements SET content = ? WHERE id = ?', [content, reqId]);
+  return query('SELECT * FROM project_requirements WHERE id = ?', [reqId])
+};
+
+const deleteRequirement = async (id) => {
+  await query('DELETE FROM reqs_to_tasks WHERE req_id = ?', [id]);
+  return await query('DELETE FROM project_requirements WHERE id = ?', [id]);
+};
+
+// Tasks
+
+const getProjectTasks = async (projectId) => {
+  return await query('SELECT * FROM project_tasks WHERE project_id = ?', [projectId]);
+};
+
+const getTask = async (taskId) => {
+  return await query('SELECT * FROM project_tasks WHERE id = ?', [taskId]);
+};
+
+const getTaskRequirements = async (taskId) => {
+  return await query('SELECT * FROM project_requirements pr JOIN reqs_to_tasks rtt ON pr.id = rtt.req_id WHERE rtt.task_id = ?', [taskId]);
+};
+
+const updateTask = async ({taskId, parentTaskId, content}) => {
+  if (parentTaskId) {
+    await query('UPDATE project_tasks SET parent_task_id = ? WHERE id = ?', [parentTaskId, taskId]);
+  } else {
+    await query('UPDATE project_tasks SET content = ? WHERE id = ?', [content, taskId]);
+  }
+
+  return query('SELECT * FROM project_tasks WHERE id = ?', [taskId])
+};
+
+const createReqToTask = async (taskId, reqId) => {
+  return await query('INSERT INTO reqs_to_tasks (task_id, req_id) VALUES (?, ?)', [taskId, reqId]);
+};
+
+const createTask = async (content, reqs, projectId, parentTaskId) => {
+  const data = await query('INSERT INTO project_tasks (content, project_id, parent_task_id) VALUES (?, ?, ?)', [content, projectId, parentTaskId]);
+  const newId = await query('SELECT max(id) as id FROM project_tasks');
+  reqs.map(reqId => createReqToTask(newId[0].id, reqId));
+};
+
+const deleteTask = async (taskId) => {
+  let newTasksToDelete = await query('SELECT * FROM project_tasks WHERE parent_task_id = ?', [taskId]);
+  let newTaskIdsToDelete = _.map(newTasksToDelete, 'id');
+  let taskIdsToDelete = [taskId, ...newTaskIdsToDelete];
+  let deleteSize = 0;
+  while (_.size(taskIdsToDelete) !== deleteSize) {
+    newTasksToDelete = await query('SELECT * FROM project_tasks WHERE parent_task_id IN (?)', [taskIdsToDelete]);
+    newTaskIdsToDelete = _.map(newTasksToDelete, 'id');
+    taskIdsToDelete = [...taskIdsToDelete, ...newTaskIdsToDelete];
+    deleteSize = _.size(taskIdsToDelete);
+  }
+  console.log('delete!', taskIdsToDelete);
+
+  await query('DELETE FROM reqs_to_tasks WHERE task_id IN (?)', [taskIdsToDelete]);
+  await query('DELETE FROM project_tasks WHERE id IN (?)', [taskIdsToDelete]);
+
+};
+
+const getTasksRequirements = async (taskIds) => {
+  return await query('SELECT * FROM project_requirements pr JOIN reqs_to_tasks rtt ON pr.id = rtt.req_id WHERE rtt.task_id IN (?)', [taskIds]);
+};
 
 module.exports = {
-  projects,
-  projectFiles,
-  projectRequirements,
-  projectTasks,
-  taskToRequirements
+  getUsers,
+  verifyUser,
+  getProjects,
+  createProject,
+  updateProject,
+  getProject,
+  deleteProject,
+  getProjectFiles,
+  addProjectFile,
+  getProjectRequirements,
+  updateRequirement,
+  deleteRequirement,
+  getProjectTasks,
+  getTask,
+  updateTask,
+  createTask,
+  createReqToTask,
+  deleteTask,
+  getTaskRequirements,
+  getTasksRequirements
 };
--- index.js
+++ index.js
@@ -3,7 +3,29 @@ const cors = require('cors');
 const multer = require('multer');
 const _ = require('lodash');
 const bodyParser = require('body-parser');
-const { projects, projectRequirements, projectFiles, projectTasks, taskToRequirements } = require('./dataAccess');
+const cookieParser = require('cookie-parser');
+const basicAuth = require('express-basic-auth');
+const {
+  getUsers,
+  verifyUser,
+  getProjects,
+  createProject,
+  updateProject,
+  getProject,
+  deleteProject,
+  getProjectFiles,
+  addProjectFile,
+  getProjectRequirements,
+  updateRequirement,
+  deleteRequirement,
+  getProjectTasks,
+  createReqToTask,
+  updateTask,
+  createTask,
+  deleteTask,
+  getTasksRequirements,
+} = require('./dataAccess');
+const {encrypt, decrypt, AUTH_TOKEN, AUTH_TOKEN_IV} = require('./authTools');
 
 const app = express();
 const port = 3010;
@@ -12,136 +34,197 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(express.static('public'));
 
+app.use(cookieParser('82e4e438a0802faaf62f9854e3b575af'));
+
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
+  destination: function async(req, file, cb) {
     cb(null, 'public/files')
   },
-  filename: function (req, file, cb) {
+  filename: function async(req, file, cb) {
     cb(null, file.originalname)
   }
 });
-const upload = multer({ storage }).single('file');
+const upload = multer({storage}).single('file');
 
-app.get('/projects', (req, res) => {
-    res.set('Access-Control-Allow-Origin', '*');
-    res.json(projects);
-});
-
-app.post('/projects', (req, res) => {
-    res.set('Access-Control-Allow-Origin', '*');
-    projects.push({
-       id: (_.get(_.maxBy(projects, 'id'), 'id')  || 0) + 1,
-       name: req.body.name
+const authMiddleware = async (req, res, next) => {
+  try {
+    const authUser = decrypt({
+      encryptedData: req.headers[AUTH_TOKEN], iv: req.headers[AUTH_TOKEN_IV]
     });
-    res.json(projects);
+    const user = await verifyUser(authUser);
+    if (user[0].count_users === 1) {
+      next();
+    } else {
+      res.send('unauthorised');
+    }
+  } catch (err) {
+    res.send('unauthorised');
+  }
+};
+
+app.post('/auth', async (req, res) => {
+  const users = await getUsers();
+  let foundUser = false;
+  users.forEach(({username, password}) => {
+    if (username === req.body.username && password === req.body.password) {
+      foundUser = encrypt(req.body.username);
+    }
+  });
+
+  res.json({result: foundUser});
 });
 
-app.put('/projects/:id', (req, res) => {
-    res.set('Access-Control-Allow-Origin', '*');
-    _.remove(projects, ({id}) => _.toString(id) === req.params.id);
-    projects.push(req.body);
-    res.json(req.body);
+app.get('/verifyAuth', async (req, res) => {
+  try {
+    const authUser = decrypt({
+      encryptedData: req.headers[AUTH_TOKEN], iv: req.headers[AUTH_TOKEN_IV]
+    });
+    const users = await getUsers();
+    res.send(_.map(users, 'username').includes(authUser));
+  } catch (err) {
+    res.send('false');
+  }
 });
 
-app.delete('/projects/:id', (req, res) => {
-    res.set('Access-Control-Allow-Origin', '*');
-    _.remove(projects, ({id}) => _.toString(id) === req.params.id);
-    res.send('ok');
-});
+// Projects
 
-app.get('/projects/:id', (req, res) => {
+app.get('/projects', authMiddleware, async (req, res) => {
   res.set('Access-Control-Allow-Origin', '*');
-  res.json(_.find(projects, ({id}) => _.toString(id) === req.params.id));
+  const authUser = decrypt({
+    encryptedData: req.headers[AUTH_TOKEN], iv: req.headers[AUTH_TOKEN_IV]
+  });
+
+  res.json(await getProjects(authUser));
 });
 
-app.get('/projectFiles/:id', (req, res) => {
+app.post('/projects', authMiddleware, async (req, res) => {
   res.set('Access-Control-Allow-Origin', '*');
-  const files = _.filter(projectFiles, ({projectId}) => _.toString(projectId) === req.params.id);
-  res.json(files || []);
+  await createProject(req.body.name);
+  res.json(await getProjects());
 });
 
-app.post('/projectFiles/:id', (req, res) => {
+app.put('/projects/:id', authMiddleware, async (req, res) => {
   res.set('Access-Control-Allow-Origin', '*');
-  upload(req, res, function (err) {
+  await updateProject(req.body);
+  res.json(req.body);
+});
+
+app.delete('/projects/:id', authMiddleware, async (req, res) => {
+  res.set('Access-Control-Allow-Origin', '*');
+  await deleteProject(req.params.id);
+  res.send('ok');
+});
+
+app.get('/projects/:id', authMiddleware, async (req, res) => {
+  res.set('Access-Control-Allow-Origin', '*');
+  res.json(await getProject(req.params.id));
+});
+
+// Files
+
+app.get('/projectFiles/:id', authMiddleware, async (req, res) => {
+  res.set('Access-Control-Allow-Origin', '*');
+  res.json(await getProjectFiles(req.params.id));
+});
+
+app.post('/projectFiles/:id', authMiddleware, async (req, res) => {
+  res.set('Access-Control-Allow-Origin', '*');
+  upload(req, res, async (err) => {
     if (err instanceof multer.MulterError) {
       return res.status(500).json(err);
     } else if (err) {
       return res.status(500).json(err);
     }
 
-    projectFiles.push({
-      id: (_.get(_.maxBy(projectFiles, 'id'), 'id')  || 0) + 1,
+    await addProjectFile({
       name: req.file.originalname,
       projectId: req.params.id
     });
+
     return res.status(200).send(req.file);
   });
 });
 
-app.get('/projectRequirements/:id', (req, res) => {
+app.get('/projectRequirements/:id', authMiddleware, async (req, res) => {
   res.set('Access-Control-Allow-Origin', '*');
-  const requirements = _.filter(projectRequirements, ({projectId}) => _.toString(projectId) === req.params.id);
-  res.json(requirements || []);
+  res.json(await getProjectRequirements(req.params.id));
 });
 
-app.put('/projectRequirements/:id', (req, res) => {
+app.put('/projectRequirements/:id', authMiddleware, async (req, res) => {
   res.set('Access-Control-Allow-Origin', '*');
-  const requirement = _.find(projectRequirements, ({id}) => _.toString(id) === req.params.id);
-  requirement.content = req.body.updatedContent;
-  res.json(requirement);
+  const requirement = await updateRequirement(req.params.id, req.body.updatedContent);
+  res.json(requirement[0]);
 });
 
-app.delete('/projectRequirement/:id', (req, res) => {
+app.delete('/projectRequirement/:id', authMiddleware, async (req, res) => {
   res.set('Access-Control-Allow-Origin', '*');
-  _.remove(projectRequirements, ({id}) => _.toString(id) === req.params.id);
+  await deleteRequirement(req.params.id);
   res.send('ok');
 });
 
+// Tasks
 
-app.get('/projectTasks/:id', (req, res) => {
+app.get('/projectTasks/:id', authMiddleware, async (req, res) => {
   res.set('Access-Control-Allow-Origin', '*');
-  const tasks = _.filter(projectTasks, ({projectId}) => _.toString(projectId) === req.params.id);
-  const requirements = _.filter(projectRequirements, ({projectId}) => _.toString(projectId) === req.params.id);
-  const tasksWithRequirements = _.map(
-    tasks,
-    (task) => ({
-      ...task,
-      requirements: _.filter(requirements, ({id}) => _.map(_.filter(taskToRequirements, ({taskId}) => taskId === task.id), 'reqId').includes(id))
+  const tasks = await getProjectTasks(req.params.id);
+  const tasksObjects = _.map(tasks, task => Object.assign({}, task));
+  const taskIds = _.map(tasks, 'id');
+
+  const reqs = await getTasksRequirements(taskIds);
+  const reqsObjects = _.map(
+    reqs, ({
+             content,
+             req_id,
+             task_id,
+             source_text,
+           }) => ({
+      reqId: req_id,
+      taskId: task_id,
+      sourceText: source_text,
+      content,
     })
   );
-  res.json(tasksWithRequirements || []);
+
+  const tasksWithRequirements = _.map(
+    tasksObjects,
+    ({id, parent_task_id, content}) => {
+      let taskReqs = _.filter(reqsObjects, ({taskId}) => taskId === id);
+      return ({
+        id,
+        parentTaskId: parent_task_id,
+        content,
+        requirements: taskReqs
+      });
+    });
+  res.json(tasksWithRequirements);
 });
 
-app.put('/projectTasks/:id', (req, res) => {
+app.put('/projectTasks/:id', authMiddleware, async (req, res) => {
   res.set('Access-Control-Allow-Origin', '*');
-  const task = _.find(projectTasks, ({id}) => _.toString(id) === req.params.id);
-  if (req.body.parentTaskId) {
-    task.parentTaskId = req.body.parentTaskId;
-  }
-  if (req.body.content) {
-    task.content = req.body.content;
-  }
+  const task = await updateTask({
+    taskId: req.params.id,
+    parentTaskId: req.body.parentTaskId,
+    content: req.body.content,
+  });
   res.json(task);
 });
 
-app.post('/linkTaskToRequirement', (req, res) => {
+app.post('/projectTasks/:projectId', authMiddleware, async (req, res) => {
   res.set('Access-Control-Allow-Origin', '*');
-  taskToRequirements.push({
-    taskId: req.body.taskId,
-    reqId: req.body.reqId,
-  });
+  console.log('debug', req.body);
+  await createTask(req.body.content, req.body.requirements, req.params.projectId, req.body.parentTaskId);
   res.json('ok');
 });
 
-app.delete('/projectTasks/:id', (req, res) => {
+app.post('/linkTaskToRequirement', authMiddleware, async (req, res) => {
   res.set('Access-Control-Allow-Origin', '*');
-  let taskIdsToDelete = [req.params.id];
-  let currentTaskIdsToDelete = _.filter(projectTasks, ({id, parentTaskId}) => req.params.id === parentTaskId);
-  while (!_.isEmpty(currentTaskIdsToDelete)) {
-    taskIdsToDelete = [...taskIdsToDelete, ...currentTaskIdsToDelete];
-    currentTaskIdsToDelete = _.filter(projectTasks, ({id, parentTaskId}) => currentTaskIdsToDelete.includes(parentTaskId));
-  }
-  _.remove(projectTasks, ({id}) => taskIdsToDelete.includes(_.toString(id)));
+  await createReqToTask(req.body.taskId, req.body.reqId);
+  res.json('ok');
+});
+
+app.delete('/projectTasks/:id', authMiddleware, async (req, res) => {
+  res.set('Access-Control-Allow-Origin', '*');
+  await deleteTask(req.params.id);
   res.send('ok');
 });
 
